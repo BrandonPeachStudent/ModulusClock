44b7363a370dde9294c9c65cacd50986d0bc848c
Brandon Peach <brandon.peach.2022@student.lhschools.org>
2020-02-25 12:30:17 -0500

    UserDataCaptured

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,16 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter an hour of time");
+        int Hour = keyboard.nextInt();
+        System.out.print("Enter minutes of time");
+        int Minute = keyboard.nextInt();
+
+
     }
 }
