bb5baa80cd73a9ee63722f57a9df03d62eb5dced
Brandon Peach <brandon.peach.2022@student.lhschools.org>
2020-02-28 11:57:53 -0500

    Ternary Operator built for Hours

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,10 +6,29 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        System.out.print("Enter an hour of time");
-        int Hour = keyboard.nextInt();
-        System.out.print("Enter minutes of time");
-        int Minute = keyboard.nextInt();
+
+        System.out.println("Enter an hour of time");
+        int hour = keyboard.nextInt();
+
+        int answer = hour >= 13 ? 1 : 0;
+        answer = hour >= 14 ? 2 : 0;
+        answer = hour >= 15 ? 3 : 0;
+        answer = hour >= 16 ? 4 : 0;
+        answer = hour >= 17 ? 5 : 0;
+        answer = hour >= 18 ? 6 : 0;
+        answer = hour >= 19 ? 7 : 0;
+        answer = hour >= 20 ? 8 : 0;
+        answer = hour >= 21 ? 9 : 0;
+        answer = hour >= 22 ? 10 : 0;
+        answer = hour >= 23 ? 11 : 0;
+        answer = hour >= 24 ? 12 : 0;
+
+        System.out.println("Enter minutes of time");
+        int minute = keyboard.nextInt();
+
+
+
+
 
 
     }
