6963bf7c3659b970b5876e4c6014038949bafb37
Brandon Peach <brandon.peach.2022@student.lhschools.org>
2020-03-03 11:58:15 -0500

    Modulus Operator for hours

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -9,24 +9,11 @@ public class Main {
 
         System.out.println("Enter an hour of time");
         int hour = keyboard.nextInt();
-
-        int answer = hour >= 13 ? 1 : 0;
-        answer = hour >= 14 ? 2 : 0;
-        answer = hour >= 15 ? 3 : 0;
-        answer = hour >= 16 ? 4 : 0;
-        answer = hour >= 17 ? 5 : 0;
-        answer = hour >= 18 ? 6 : 0;
-        answer = hour >= 19 ? 7 : 0;
-        answer = hour >= 20 ? 8 : 0;
-        answer = hour >= 21 ? 9 : 0;
-        answer = hour >= 22 ? 10 : 0;
-        answer = hour >= 23 ? 11 : 0;
-        answer = hour >= 24 ? 12 : 0;
-
-        System.out.println("Enter minutes of time");
-        int minute = keyboard.nextInt();
+        hour = (hour % 12);
 
 
+
+        
 
 
 
